61b97f24058896c5bf50712e18f1d185349d2cb6
cs <csfromcs@gmail.com>
2023-06-08 17:38:55 +0800

    upload answer to day 1

---
+++ Introduction to Python/PythonDev/Student/03-FlowControl/dateprocessing.py
@@ -0,0 +1,74 @@
+﻿# Ask the user for a day, month, and year.
+day   = int(input("Enter a day (1-31): "))
+month = int(input("Enter a month (1-12): "))
+year  = int(input("Enter a year (0 to 2099): "))
+
+# Add the rest of your code here.
+
+valid = True
+# validate year
+if(year < 0 or year > 2099):
+    valid = False
+    
+
+# is leap?
+leap_year = False
+if((year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)):
+    print(f"{year} is a leap year!")
+    leap_year = True
+else:
+    print(f"{year} is NOT a leap year.")
+    leap_year = False
+    
+    
+# validate month
+if(month < 1 or month > 12):
+    valid = False
+
+    
+# validate day
+nonleap = [31,28,31,30,31,30,31,31,30,31,30,31]
+leap = [31,29,31,30,31,30,31,31,30,31,30,31]
+
+if(leap_year):
+    if((day < 0) or (day > leap[month-1])):
+        valid = False
+else:
+    if((day < 0) or (day > nonleap[month-1])):
+        valid = False
+
+
+    
+if(valid):
+    print(f"{day}/{month}/{year} is a valid date!")
+else:
+    print(f"{day}/{month}/{year} is NOT a valid date.")
+    
+    
+months = ["January","February","March","April","May", "June", "July", "August", "September", "October", "November","December"]
+    
+print("\n-----------\n")
+print(f"All days in {months[month-1]} {year}")
+
+if(leap_year):
+    for i in range(1,leap[month-1]+1):
+        if(i%10 == 1):
+            sfx = "st"
+        elif(i%10 == 2):
+            sfx = "nd"
+        elif(i%10 == 3):
+            sfx = "rd"
+        else:
+            sfx = "th"
+        print(f"{i}{sfx} {months[month-1]} {year}")
+else:
+    for i in range(1,nonleap[month-1]+1):
+        if(i%10 == 1):
+            sfx = "st"
+        elif(i%10 == 2):
+            sfx = "nd"
+        elif(i%10 == 3):
+            sfx = "rd"
+        else:
+            sfx = "th"
+        print(f"{i}{sfx} {months[month-1]} {year}")
---
+++ Introduction to Python/PythonDev/Student/04-Functions/dateprocessing.py
@@ -0,0 +1,125 @@
+﻿# Ask the user for a day, month, and year.
+day   = int(input("Enter a day (1-31): "))
+month = int(input("Enter a month (1-12): "))
+year  = int(input("Enter a year (0 to 2099): "))
+
+# Determine if it's a leapyear.
+def isLeapYear(year):
+    isLeapYear = ((year % 4 == 0) and not (year %
+                  100 == 0)) or (year % 400 == 0)
+    return isLeapYear
+
+# Determine if it's a valid date.
+
+
+def getDaysInMonth(month, year):
+    if month == 2:
+        daysInMonth = 29 if isLeapYear else 28
+    elif month in (4, 6, 9, 11):
+        daysInMonth = 30
+    else:
+        daysInMonth = 31
+    return daysInMonth
+
+
+def isValidDate(day, month, year):
+    daysInMonth = getDaysInMonth(month, year)
+    isvalid = day >= 1 and day <= daysInMonth and \
+        month >= 1 and month <= 12 and \
+        year >= 0 and year <= 2099
+    return isvalid
+
+# print("%02d/%02d/%04d valid? %s" % (day, month, year, isvalid))
+
+# Determine the name of the month.
+
+
+def getMonthName(month):
+    if month == 1:
+        monthName = "January"
+    elif month == 2:
+        monthName = "February"
+    elif month == 3:
+        monthName = "March"
+    elif month == 4:
+        monthName = "April"
+    elif month == 5:
+        monthName = "May"
+    elif month == 6:
+        monthName = "June"
+    elif month == 7:
+        monthName = "July"
+    elif month == 8:
+        monthName = "August"
+    elif month == 9:
+        monthName = "September"
+    elif month == 10:
+        monthName = "October"
+    elif month == 11:
+        monthName = "November"
+    elif month == 12:
+        monthName = "December"
+    else:
+        monthName = "Not Known"     # Should never happen
+
+    return monthName
+
+
+def getDaySuffix(day):
+    if day in (1, 21, 31):
+        suffix = "st"
+    elif day in (2, 22):
+        suffix = "nd"
+    elif day in (3, 23):
+        suffix = "rd"
+    else:
+        suffix = "th"
+    return suffix
+
+# Display all the dates in the month.
+
+
+def displayAllDatesInMonth(month, year, verbose=False):
+    daysInMonth = getDaysInMonth(month, year)
+
+    monthName = getMonthName(month)
+
+    for day in range(1, daysInMonth+1):
+        suffix = getDaySuffix(day)
+        if (verbose):
+            print("%d%s %s %d" % (day, suffix, monthName, year))
+        else:
+            print(f"{day:02.0f}/{month:02.0f}/{year}")
+
+
+# print("Dates in %s, %d" % (monthName, year))
+displayAllDatesInMonth(month, year)
+
+# for day in range(1, daysInMonth+1):
+#     if day in (1, 21, 31):
+#         suffix = "st"
+#     elif day in (2, 22):
+#         suffix = "nd"
+#     elif day in (3, 23):
+#         suffix = "rd"
+#     else:
+#         suffix = "th"
+
+#     print("%d%s %s %d" % (day , suffix, monthName, year))
+
+def displaySpecialDatesInMonth(month, year, *args, verbose=False):
+    daysInMonth = getDaysInMonth(month, year)
+
+    monthName = getMonthName(month)
+
+    for day in args:
+        suffix = getDaySuffix(day)
+        if (verbose):
+            print("%d%s %s %d" % (day, suffix, monthName, year))
+        else:
+            print(f"{day:02.0f}/{month:02.0f}/{year}")
+
+
+displaySpecialDatesInMonth(12, year, 3, 25, 26, 31, verbose=True)
+
+displaySpecialDatesInMonth(12, year, 3, 25, 26, 31)
